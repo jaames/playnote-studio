83b9fbbb5b6484505945537dabd5df5bba655716
jaames <jamesdaniel2197@gmail.com>
2022-01-26 23:43:11 +0000

    website init

---
+++ website/astro.config.mjs
@@ -0,0 +1,37 @@
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+  // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
+  // pages: './src/pages', // Path to Astro components, pages, and data
+  // dist: './dist',       // When running `astro build`, path to final static output
+  // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
+  buildOptions: {
+    // site: 'http://example.com',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+    sitemap: true,         // Generate sitemap (set to "false" to disable)
+  },
+  devOptions: {
+    // hostname: 'localhost',  // The hostname to run the dev server on.
+    // port: 3000,             // The port to run the dev server on.
+  },
+  renderers: [
+    "@astrojs/renderer-preact"
+  ],
+  vite: {
+    resolve: {
+      alias: {
+        '@/': `${path.resolve(__dirname, 'src')}/`
+      }
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "@/styles/vars.scss";`
+        }
+      }
+    }
+  }
+};
---
+++ website/readme.md
@@ -0,0 +1,14 @@
+# playnote.studio website
+
+Built with [astro](https://astro.build)
+
+## Commands
+
+All commands are run from the /website directory, from a terminal:
+
+| Command           | Action                                       |
+|:----------------  |:-------------------------------------------- |
+| `npm install`     | Installs dependencies                        |
+| `npm run dev`     | Starts local dev server at `localhost:3000`  |
+| `npm run build`   | Build your production site to `./website/dist/`      |
+| `npm run preview` | Preview your build locally, before deploying |
